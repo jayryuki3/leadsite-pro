7c7a79f1f60a5368397f245f39f753e2d6088c90
jayryuki3 <jayryuki3@gmail.com>
2026-03-09 17:04:16 -0700

    Add Axios API client

---
+++ frontend/src/api/client.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+
+const api = axios.create({
+  baseURL: '/api',
+  timeout: 60000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 500) {
+      console.error('Server error:', error.response.data)
+    }
+    return Promise.reject(error)
+  }
+)
+
+export default api
