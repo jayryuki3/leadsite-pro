4b6d6d857f5a6b502a44739838845af9f97b26eb
jayryuki3 <jayryuki3@gmail.com>
2026-03-09 17:03:54 -0700

    Add App component with routing

---
+++ frontend/src/App.jsx
@@ -0,0 +1,27 @@
+import { Routes, Route } from 'react-router-dom'
+import Sidebar from './components/Sidebar'
+import Dashboard from './pages/Dashboard'
+import Discover from './pages/Discover'
+import Leads from './pages/Leads'
+import MockupBuilder from './pages/MockupBuilder'
+import Outreach from './pages/Outreach'
+import Settings from './pages/Settings'
+
+export default function App() {
+  return (
+    <div className="flex h-screen bg-gray-50">
+      <Sidebar />
+      <main className="flex-1 overflow-auto">
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/discover" element={<Discover />} />
+          <Route path="/leads" element={<Leads />} />
+          <Route path="/mockups" element={<MockupBuilder />} />
+          <Route path="/mockups/:id" element={<MockupBuilder />} />
+          <Route path="/outreach" element={<Outreach />} />
+          <Route path="/settings" element={<Settings />} />
+        </Routes>
+      </main>
+    </div>
+  )
+}
